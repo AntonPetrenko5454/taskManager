7feed12c4279b4284406cb2b59bc0b0a822fd538
AntonPetrenko5454 <toha20055454@mail.ru>
2022-11-05 16:20:36 +0300

    05.11.2022

--- keyboards/main.py
+++ keyboards/main.py
@@ -3,4 +3,4 @@ from aiogram import types
 mainKeyboard = types.InlineKeyboardMarkup(row_width=1)
 userInfoButton = types.InlineKeyboardButton(text='Информация о пользователе', callback_data='userInfoButton_click')
 
-mainKeyboard.add('''getTaskButton''', '''giveTaskButton''', userInfoButton, '''searchUserButton''', '''botInfoButton''', '''exitButton''')
+mainKeyboard.add( userInfoButton)
